778e363d54c5c0474604d635efe17413d3179ed4
chasj0326 <62418379+chasj0326@users.noreply.github.com>
2021-06-21 00:06:42 +0900

    0620

---
+++ scriptfile.js
@@ -0,0 +1,65 @@
+function mover1(){
+		var T1, title1, title2, t1, t2, t;
+		title1=document.getElementById("title1");
+		title2=document.getElementById("title2");
+		t=document.getElementById("text1");
+		t1=document.getElementById("text1-1");
+		t2=document.getElementById("text1-2");
+		sub=document.getElementById("sub");
+		T1=document.getElementById("tool1");
+		
+		t.style.color="rgba(145, 162, 189, 0)";
+		t1.style.color="rgba(0, 0, 0, 1)";
+		t2.style.color="rgba(51, 114, 217, 1)";
+		title1.style.color="white";
+		title2.style.color="white";
+		T1.style.backgroundColor="rgba(101, 160, 255, 1)";
+	}
+	function mout1(){
+		var T1, title1, title2, t1, t2, t;
+		title1=document.getElementById("title1");
+		title2=document.getElementById("title2");
+		t=document.getElementById("text1");
+		t1=document.getElementById("text1-1");
+		t2=document.getElementById("text1-2");
+		T1=document.getElementById("tool1");
+		
+		t.style.color="rgba(145, 162, 189, 1)"
+		t1.style.color="rgba(0, 0, 0, 0)";
+		t2.style.color="rgba(0, 0, 0, 0)";
+		title1.style.color="#2B2A2A";
+		title2.style.color="#2B2A2A";
+		T1.style.backgroundColor="rgba(101, 160, 255, 0.35)";
+	}
+	function mover2(){
+		var T2, title3, title4, t1, t2, t;
+		title3=document.getElementById("title3");
+		title4=document.getElementById("title4");
+		t=document.getElementById("text2");
+		t1=document.getElementById("text2-1");
+		t2=document.getElementById("text2-2");
+		T2=document.getElementById("tool2");
+		
+		t.style.color="rgba(159, 122, 178, 0)";
+		t1.style.color="rgba(0, 0, 0, 1)";
+		t2.style.color="rgba(190, 72, 255, 1)";
+		title3.style.color="white";
+		title4.style.color="white";
+		T2.style.backgroundColor="rgba(209, 125, 255, 1)";
+	}
+	function mout2(){
+		var T2, title3, title4, t, t1, t2;
+		title3=document.getElementById("title3");
+		title4=document.getElementById("title4");
+		t=document.getElementById("text2");
+		t1=document.getElementById("text2-1");
+		t2=document.getElementById("text2-2");
+		T2=document.getElementById("tool2");
+		
+		t.style.color="rgba(159, 122, 178, 1)";
+		t1.style.color="rgba(0, 0, 0, 0)";
+		t2.style.color="rgba(0, 0, 0, 0)";
+		title3.style.color="#2B2A2A";
+		title4.style.color="#2B2A2A";
+		T2.style.backgroundColor="rgba(209, 125, 255, 0.35)";
+	}
